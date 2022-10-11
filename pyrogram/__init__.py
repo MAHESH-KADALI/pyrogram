bbb6f360ec28baaba890fbcd713aaf81026f5034
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-10-11 20:36:43 +0530

    Update __init__.py

--- pyrogram/__init__.py
+++ pyrogram/__init__.py
@@ -1,7 +1,7 @@
 #  Pyrogram - Telegram MTProto API Client Library for Python
 #  Copyright (C) 2017-present Dan <https://github.com/delivrance>
 #
-#  This file is part of Pyrogram.
+#  This file is part of Pyrogram. 
 #
 #  Pyrogram is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published
